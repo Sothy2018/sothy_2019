8048e046d67aefe054865bba4230ca3ba8fd153c
Sothy2018 <35627869+Sothy2018@users.noreply.github.com>
2019-12-21 08:55:06 +0700

    Add files via upload

--- x-run.R
+++ x-run.R
@@ -1,13 +1,23 @@
 
 library(ggplot2)
 library(readr)
+library(dplyr)
+library(reshape2)
 
 d <- read_csv("x-run.csv")
 
-ggplot(d, aes(x = n, y = Time, color = Tool)) +
-  geom_point() + geom_line() + scale_x_log10(breaks = c(0.01,0.1,1,10)) + scale_y_log10()
+d %>% select(1:4) %>% melt(id.vars=1:2) %>%
+  ggplot(aes(x = n, y = value, color = Tool)) +
+  geom_point() + geom_line() + 
+  facet_wrap(~variable, scales = "free", ncol = 2) +
+  scale_x_log10(breaks = c(0.01,0.1,1,10)) + 
+  scale_y_log10(breaks = c(1,10,100,1000,10000))
 
-ggplot(d, aes(x = n, y = AUC, color = Tool)) +
-  geom_point() + geom_line() + scale_x_log10(breaks = c(0.01,0.1,1,10))
+d %>% select(c(1,2,5,6)) %>% melt(id.vars=1:2) %>%
+  ggplot(aes(x = n, y = value, color = Tool)) +
+  geom_point() + geom_line() + 
+  facet_wrap(~variable, scales = "free", ncol = 2) +
+  scale_x_log10(breaks = c(0.01,0.1,1,10)) +
+  scale_y_continuous(breaks = seq(60,80,2))
 
 
